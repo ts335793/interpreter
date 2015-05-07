4ca71e6d6541846924d1fd1a5cb931c357f62551
Tomasz Stęczniewski <tomaszsteczniewski@gmail.com>
2015-05-07 17:32:06 +0200

    Come on.

--- TypeInterference2.hs
+++ TypeInterference2.hs
@@ -62,13 +62,17 @@ setSubstitutions subs = do
   (x, _) <- lift get
   lift $ put (x, subs)
 
+-- rozw
 setSubstitution :: Label -> Type -> IM ()
-setSubstitution l t = do
-  subs <- getSubstitutions
-  let subs' = Map.insert l t subs
-      subs'' = Map.map (applySubstitutions subs') subs'
-  setSubstitutions subs''
+setSubstitution l t
+  | TVar l == t = return ()
+  | otherwise = do
+    subs <- getSubstitutions
+    let subs' = Map.insert l t subs
+        subs'' = Map.map (applySubstitutions subs') subs'
+    setSubstitutions subs''
 
+-- rozw
 containsLabel :: Label -> Type -> IM Bool
 containsLabel _ TInt = return False
 containsLabel _ TBool = return False
@@ -78,21 +82,24 @@ containsLabel l (TVar x)
   | otherwise = return False
 containsLabel l (TList t) = containsLabel l t
 
+-- nie rozw 1 -> rozw
 applySubstitutions :: (Map Label Type) -> Type -> Type
 applySubstitutions subs TInt = TInt
 applySubstitutions subs TBool = TBool
 applySubstitutions subs (from :-> to) = ((applySubstitutions subs from) :-> (applySubstitutions subs to))
 applySubstitutions subs (TVar x) =
   case Map.lookup x subs of
-    Just xs -> applySubstitutions subs xs
+    Just xs -> xs
     Nothing -> TVar x
-applySubstitutions subs (TList t) = TList $ applySubstitutions subs t
+applySubstitutions subs (TList t) = TList (applySubstitutions subs t)
 
+-- nie rozw 1 -> rozw
 applySubstitutionsM :: Type -> IM Type
 applySubstitutionsM t = do
   subs <- getSubstitutions
   return $ applySubstitutions subs t
 
+-- rozw -> rozw
 unificate :: Type -> Type -> IM Type
 unificate TInt TInt = return TInt
 unificate TBool TBool = return TBool
@@ -133,3 +140,101 @@ instantiate (Forall vs t) = do
   subs <- sequence $ Map.fromSet (const newLabel) vs
   return $ applySubstitutions subs t
 
+class Typeable a where
+  typeOf :: a -> IM Type
+
+instance Typeable Param where
+  typeOf (PInt x) = typeOf (EInt x)
+  typeOf (PApp1 e) = typeOf (EApp1 e [])
+  typeOf (PApp2 e) = typeOf (EApp2 e [])
+  typeOf (PListConst1 l) = typeOf (EListConst1 l)
+
+emptyQType :: Type -> QType
+emptyQType t = Forall Set.empty t
+
+envInsert :: Ident -> QType -> Env -> Env
+envInsert x qt (Env e) = Env (Map.insert x qt e)
+
+envGet :: Ident -> Env -> QType
+envGet i (Env e) = e Map.! i
+
+typeOfBB e = do
+  et <- typeOf e
+  unificate et TBool
+  return TBool
+typeOfABC a b c e1 e2 = do
+  e1t <- typeOf e1
+  e2t <- typeOf e2
+  unificate e1t a
+  unificate e2t b
+  return c
+typeOfBBB e1 e2 = typeOfABC TBool TBool TBool e1 e2
+typeOfIIB e1 e2 = typeOfABC TInt TInt TBool e1 e2
+typeOfIII e1 e2 = typeOfABC TInt TInt TInt e1 e2
+
+-- -> rozw
+instance Typeable Exp where
+  -- Exp
+  typeOf (ELet x params body e) = do
+    xt <- newLabel
+    bodyt <- local (envInsert x (emptyQType xt)) (typeOf (ELam params body))
+    unificate xt bodyt
+    gxt <- generalize xt
+    local (envInsert x gxt) (typeOf e)
+  typeOf (EIf e1 e2 e3) = do
+    e1t <- typeOf e1
+    e2t <- typeOf e2
+    e3t <- typeOf e3
+    unificate e1t TBool
+    unificate e2t e3t
+  typeOf (ELam [] e) = typeOf e
+  typeOf (ELam (param:params) e) = do
+    paramt <- newLabel
+    et <- local (envInsert param (emptyQType paramt)) (typeOf (ELam params e))
+    applySubstitutionsM (paramt :-> et)
+  -- Exp1
+  typeOf (ENot e) = typeOfBB e
+  typeOf (EAnd e1 e2) = typeOfBBB e1 e2
+  typeOf (EOr e1 e2) = typeOfBBB e1 e2
+  typeOf (EEq e1 e2) = typeOfIIB e1 e2
+  typeOf (ENeq e1 e2) = typeOfIIB e1 e2
+  typeOf (ELeq e1 e2) = typeOfIIB e1 e2
+  typeOf (EGeq e1 e2) = typeOfIIB e1 e2
+  typeOf (ELt e1 e2) = typeOfIIB e1 e2
+  typeOf (EGt e1 e2) = typeOfIIB e1 e2
+  -- Exp2
+  typeOf (EPlus e1 e2) = typeOfIII e1 e2
+  typeOf (EMinus e1 e2) = typeOfIII e1 e2
+  -- Exp3
+  typeOf (ETimes e1 e2) = typeOfIII e1 e2
+  typeOf (EObelus e1 e2) = typeOfIII e1 e2
+  -- Exp4
+  typeOf (EInt _) = return TInt
+  typeOf (EApp1 f params) = do
+    ft <- typeOf f
+    foldM (\acc param -> do
+      paramt <- typeOf param
+      ot <- newLabel
+      unificate acc (paramt :-> ot)
+      return ot) ft params
+  typeOf (EApp2 f params) = do
+    fqt <- asks (envGet f)
+    ft <- instantiate fqt
+    foldM (\acc param -> do
+      paramt <- typeOf param
+      ot <- newLabel
+      unificate acc (paramt :-> ot)
+      return ot) ft params
+  typeOf (EListConst1 elems) = do
+    t <- newLabel
+    foldM (\acc elem -> do
+      elemt <- typeOf elem
+      unificate acc (TList elemt)
+      return acc) (TList t) elems
+  typeOf (EListConst2 p1 p2) = do
+    p1t <- typeOf p1
+    p2t <- typeOf p2
+    t <- newLabel
+    unificate p2t (TList t)
+    unificate (TList p1t) p2t
+    return (TList p1t)
