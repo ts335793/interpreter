6978b778dd866811ba4b564a4931af67c6cbe49d
Tomasz Stęczniewski <tomaszsteczniewski@gmail.com>
2015-05-08 12:13:07 +0200

    Added 2 tests.

--- Main.hs
+++ Main.hs
@@ -1,7 +1,6 @@
 module Main where
 
-import System.IO ( stdin, hGetContents )
-import System.Environment ( getArgs, getProgName )
+import System.Environment (getArgs)
 
 import BNFC.LexLanguage
 import BNFC.ParLanguage
@@ -17,37 +16,36 @@ type ParseFun a = [Token] -> Err a
 
 type Verbosity = Int
 
-putStrV :: Verbosity -> String -> IO ()
-putStrV v s = if v > 1 then putStrLn s else return ()
 
-runFile :: Verbosity -> ParseFun Exp -> FilePath -> IO ()
-runFile v p f = putStrLn f >> readFile f >>= run v p
+runFile :: ParseFun Exp -> FilePath -> IO ()
+runFile p f = putStrLn f >> readFile f >>= run p
 
-run :: Verbosity -> ParseFun Exp -> String -> IO ()
-run v p s =
+run :: ParseFun Exp -> String -> IO ()
+run p s =
   let ts = myLexer s 
   in case p ts of
     Bad s -> do
       putStrLn "\nParse Failed...\n"
-      putStrV v "Tokens:"
-      putStrV v $ show ts
+      putStrLn "Tokens:"
+      print ts
       putStrLn s
     Ok tree -> do
       putStrLn "\nParse Successful!"
-      showTree v tree
+      showTree tree
       putStrLn "\nRunning type checker..."
       print (runTypeOf tree)
       putStrLn "\nEvaluating program..."
       print (runEval tree)
 
-showTree :: (Show a, Print a) => Int -> a -> IO ()
-showTree v tree = do
-  putStrV v $ "\n[Abstract syntax]\n\n" ++ show tree
-  putStrV v $ "\n[Linearized tree]\n\n" ++ printTree tree
+showTree :: (Show a, Print a) => a -> IO ()
+showTree tree = do
+  putStrLn $ "\n[Abstract syntax]\n\n" ++ show tree
+  putStrLn $ "\n[Linearized tree]\n\n" ++ printTree tree
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [f] -> runFile 2 pExp f
+    [f] -> runFile pExp f
     _ -> print "usage: runhaskell Main.hs source_file"
+
